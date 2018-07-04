a5a020ac0149128a6129e92764884407c8e6e745
Valère Pique <pique.valere@gmx.fr>
2018-07-04 16:55:01 +0200

    Remove useless console.log

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-  console.log(message.content);
   if (!message.content.startsWith(prefix) || message.author.bot) return;
 
   const args = message.content.slice(prefix.length + 1).split(/ +/);
