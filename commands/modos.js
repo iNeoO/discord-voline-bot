3ecb552cac9f737172e0df4c9c3f29338008de08
Valère Pique <pique.valere@gmx.fr>
2018-08-05 18:41:01 +0200

    Set to 3 number of modos

--- commands/modos.js
+++ commands/modos.js
@@ -22,28 +22,35 @@ module.exports = {
       }).map(m => m.id);
       let member0;
       let member1;
-      if (membersId.length < 3) {
+      let member2;
+      if (membersId.length < 4) {
         member0 = message.guild.members.get(membersId[0]);
         member1 = message.guild.members.get(membersId[1]);
+        member2 = message.guild.members.get(membersId[2]);
         member0.addRole(moderatorRole).catch(console.error);
         member1.addRole(moderatorRole).catch(console.error);
+        member2.addRole(moderatorRole).catch(console.error);
       } else {
         let modo1 = -1;
         let modo2 = -1;
+        let modo3 = -1;
         do {
           modo1 = Math.floor(Math.random() * membersId.length);
           modo2 = Math.floor(Math.random() * membersId.length);
-        } while (modo1 === modo2);
+          modo3 = Math.floor(Math.random() * membersId.length);
+        } while (modo1 === modo2 || modo3 === modo1 || modo3 === modo2);
         member0 = message.guild.members.get(membersId[modo1]);
         member1 = message.guild.members.get(membersId[modo2]);
+        member2 = message.guild.members.get(membersId[modo3]);
         member0.addRole(moderatorRole).catch(console.error);
         member1.addRole(moderatorRole).catch(console.error);
+        member2.addRole(moderatorRole).catch(console.error);
       }
 
       const embed = new Discord.RichEmbed()
         .setTitle('Random modos log')
         .setColor('#5599ff')
-        .setDescription(`${member0.user.username} and ${member1.user.username} are now moderator`)
+        .setDescription(`${member0.user.username}, ${member1.user.username} and ${member2.user.username} are now moderator`)
         .setFooter(`Random moderators launched by: ${message.author.username}`, `${message.author.avatarURL}`);
 
       const channel = message.client.channels.find('id', annoncesIdChannel);
