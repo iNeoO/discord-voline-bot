5c2916e5c446c83795bc7571177bd24194d4e478
Powi <powi@powi.fr>
2020-07-11 23:35:58 +0200

    Update kick.js

--- src/commands/administration/kick.js
+++ src/commands/administration/kick.js
@@ -52,14 +52,8 @@ class Kick extends Command {
               target.kick();
               return msg.reply(`<@${target.id}> has been kicked !`);
             }, 5000);
-            const background = await Jimp.read('./static/img/entry_denied.png');
-            const avatar = await Jimp.read(user.avatarURL || './static/img/avatar-discord.jpg');
-            avatar.resize(70, 70);
-            const font = await Jimp.loadFont('./static/font/04b_03-16-2.fnt');
-            const image = await background.composite(avatar, 130, 180)
-              .print(font, 15, 157, user.username)
-              .getBufferAsync(Jimp.MIME_PNG);
-            return msg.channel.send(`<@${user.id}> has been denied`, new Attachment(image));
+            
+            return msg.channel.send(`<@${user.id}> has been denied`);
           } else {
             msg.reply('This user can\'t be kicked.');
           }
